dd735ec73d704bd500fd19d935a2c76ada435c6c
venky477 <113276965+venky477@users.noreply.github.com>
2022-12-01 12:34:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello raju !'));
+app.get('/', (req, res) => res.send('Hello rani !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
